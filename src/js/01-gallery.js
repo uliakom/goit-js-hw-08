1cdcd026eae9205721a5cf062e6d28d5164301c6
Uliana Komarynska <uliakom@gmail.com>
2022-05-06 13:38:22 +0300

    Task 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,27 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(imgElements) {
+    return imgElements
+        .map(({ preview, original, description }) => {
+            return   `<li><a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt="${description}" />
+</a></li>
+`                                        ;  
+    }
+    ).join(''); 
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+});
