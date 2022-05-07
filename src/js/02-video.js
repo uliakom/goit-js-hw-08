7a53e5f64f5967ac71e58d2de2653a6fa3f24611
Uliana Komarynska <uliakom@gmail.com>
2022-05-07 23:16:38 +0300

    Task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,24 +4,22 @@ import throttle from 'lodash/throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+
+const STORAGE_KEY = "videoplayer-current-time";
     
 
     player.on('play', function() {
         console.log('played the video!');
     });
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
-
 const onCurrentTimeSave = function (data) {
-    localStorage.setItem("videoplayer-current-time", data.seconds);
+    localStorage.setItem(STORAGE_KEY, data.seconds);
    console.log(data);
 };
 
 player.on('timeupdate', throttle(onCurrentTimeSave,1000));
 
-const currentTime = localStorage.getItem("videoplayer-current-time");
+const currentTime = localStorage.getItem(STORAGE_KEY);
 
 player.setCurrentTime(currentTime).then(function (seconds) {
     seconds = currentTime;
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
 
+import throttle from 'lodash/throttle';
+
+const formEmail = document.querySelector('input');
+const formMessage = document.querySelector('textarea');
+const feedbackForm = document.querySelector('.feedback-form');
+
+feedbackForm.addEventListener('input', throttle(onInputChange,500));
+feedbackForm.addEventListener('submit', onFormSubmit);
+
+const STORAGE_KEY = "feedback-form-state";
+
+let userInputData = {};
+inputDataSave();
+ 
+
+function onInputChange(event) {
+    const {
+        elements: { email, message },
+    } = event.currentTarget;
+
+    userInputData = {
+        email: email.value,
+        message: message.value
+    };
+    if (email.value === "" || message.value === "") {
+        console.log("Please fill all fields");
+    }
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(userInputData));
+    }
+    
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function inputDataSave() {
+    const savedData = localStorage.getItem(STORAGE_KEY);
+    if (savedData) {
+        const parcedData = JSON.parse(savedData);
+        formEmail.value = parcedData.email;
+        formMessage.value = parcedData.message;
+    }
+}
+
